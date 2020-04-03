4e6a18f3f0bb09e33afd1af8a8817272843735c2
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-03 17:53:42 -0300

    added form for adding new items through POST request

--- functional_tests.py
+++ functional_tests.py
@@ -31,25 +31,34 @@ class NewVisitorTest(unittest.TestCase):
     # Ela digita "Comprar penas de pavão" em uma caixa de texto 
     # (O hobby de Edith é fazer iscas para pesca com fly)
     inputbox.send_keys('Buy peacock feathers')
-
-    # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
-    # item. Ela insere "Usar penas de pavão para fazer uma isca" 
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
 
+    # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
+    # item. Ela insere "Usar penas de pavão para fazer uma isca" 
     table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_element_by_tag_name('tr')
-    self.assertTrue(
-      any(row.text == '1 : Buy peacock feathers' for row in rows),
-      "New to-do item did not appear in table"
-    )
+    rows = table.find_elements_by_tag_name('tr')
+
+    self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+    
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
 
     # A página é atualizada novamente e agora mostra os dois itens em sua lista
-    self.fail('Finish the test')
+    table = self.browser.find_element_by_id('id_list_table') 
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+    self.assertIn(
+      '2: Use peacock feathers to make fly',
+      [row.text for row in rows]
+    )
 
     # Edith se pergunta se o site se lembrará das suas notas. Então ela percebe
     # que o site gerou um URL único para ela -- há um pequeno texto explicativo 
     # para isso
+    self.fail('Finish the test')
 
     # Ela acessa esse URL - sua lista de tarefas continua lá
   
