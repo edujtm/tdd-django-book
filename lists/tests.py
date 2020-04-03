13f2b5766e4e7fd8f8515631ec7adb871408ec4a
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-02 22:39:22 -0300

    Adding first passing test, urls.py link to view and a dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
 class SmokeTest(TestCase):
 
-  def test_quick_maths(self):
-    self.assertEqual(2+2, 3)
+  def test_root_url_resolves_to_home_page_view(self):
+      found = resolve("/")
+      self.assertEqual(found.func, home_page)
