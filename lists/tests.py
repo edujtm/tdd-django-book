b8d5a560e902d72c3f1bde456c8f2749e1b45627
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-04 17:54:00 -0300

    New endpoint for creating new lists. Simplified unit tests

--- lists/tests.py
+++ lists/tests.py
@@ -14,23 +14,6 @@ class SmokeTest(TestCase):
     response = self.client.get('/')
     self.assertTemplateUsed(response, 'home.html')
 
-  def test_can_save_a_POST_request(self):
-    response = self.client.post('/', data={'item_text': 'A new list item'})
-
-    self.assertEqual(Item.objects.count(), 1, "Item was not saved into database via POST request")
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, "A new list item", "Item saved into database with wrong text")
-
-  def test_redirects_after_POST(self):
-    response = self.client.post('/', data={'item_text': 'A new list item'})
-
-    self.assertEqual(response.status_code, 302, 'Did not redirect after POST request on home page')
-    self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/', 'Redirect to wrong location after post request on main page')
-
-  def test_only_saves_item_when_necessary(self):
-    self.client.get('/')
-    self.assertEqual(Item.objects.count(), 0, "Saved item in a GET request to home page")
-
 
 class ItemModelTest(TestCase):
 
@@ -66,3 +49,15 @@ class ListViewTest(TestCase):
 
       self.assertContains(response, 'itemey 1')
       self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+  def test_can_save_a_POST_request(self):
+    response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+    self.assertEqual(Item.objects.count(), 1, "Item was not saved into database via POST request")
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, "A new list item", "Item saved into database with wrong text")
+
+  def test_redirects_after_POST(self):
+    response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
