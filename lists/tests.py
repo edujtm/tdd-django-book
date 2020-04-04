41eabdfe7ce42efe888c42cd8e10d644fa53ab42
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-04 19:20:05 -0300

    Adding List model and adapting tests to it

--- lists/tests.py
+++ lists/tests.py
@@ -4,60 +4,72 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
+
 
 # Create your tests here.
 
 class SmokeTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-  def test_home_page_returns_correct_html(self):
-    response = self.client.get('/')
-    self.assertTemplateUsed(response, 'home.html')
+
+class ListAndItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2, 'Could not save multiple itens on database')
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item',
+                         'to-do item was saved with incorrect text')
+        self.assertEqual(first_saved_item.list, list_, 'to-do item was not saved into correct list')
+        self.assertEqual(second_saved_item.text, 'Item the second', 'to-do item was saved with incorrect text')
+        self.assertEqual(second_saved_item.list, list_, 'to-do item was not saved into correct list')
 
 
-class ItemModelTest(TestCase):
-
-  def test_saving_and_retrieving_items(self):
-    first_item = Item()
-    first_item.text = "The first (ever) list item"
-    first_item.save()
-
-    second_item = Item()
-    second_item.text = 'Item the second'
-    second_item.save()
-
-    saved_items = Item.objects.all()
-    self.assertEqual(saved_items.count(), 2)
-
-    first_saved_item = saved_items[0]
-    second_saved_item = saved_items[1]
-
-    self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-    self.assertEqual(second_saved_item.text, 'Item the second')
-  
 class ListViewTest(TestCase):
+    def test_uses_list_templates(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
-  def test_uses_list_templates(self):
-    response = self.client.get('/lists/the-only-list-in-the-world/')
-    self.assertTemplateUsed(response, 'list.html')
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
-  def test_displays_all_items(self):
-      Item.objects.create(text='itemey 1')
-      Item.objects.create(text='itemey 2')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-      response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
-      self.assertContains(response, 'itemey 1')
-      self.assertContains(response, 'itemey 2')
 
 class NewListTest(TestCase):
-  def test_can_save_a_POST_request(self):
-    response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
-    self.assertEqual(Item.objects.count(), 1, "Item was not saved into database via POST request")
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, "A new list item", "Item saved into database with wrong text")
+        self.assertEqual(Item.objects.count(), 1, "Item was not saved into database via POST request")
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item", "Item saved into database with wrong text")
 
-  def test_redirects_after_POST(self):
-    response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-from lists.models import Item
-# Create your views here.
+from lists.models import Item, List
+
 
 def home_page(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def new_list(request):
-  Item.objects.create(text=request.POST['item_text'])
-  return redirect('/lists/the-only-list-in-the-world/')
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
+
 
 def view_list(request):
-  items = Item.objects.all()
-  return render(request, 'list.html', { 'items': items })
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
