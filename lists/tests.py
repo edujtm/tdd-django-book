9ee67df839ff61f3ad22634431185714d03b69dc
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-03 21:17:26 -0300

    Redirecting after POST, adding list numeration

--- lists/tests.py
+++ lists/tests.py
@@ -16,8 +16,30 @@ class SmokeTest(TestCase):
 
   def test_can_save_a_POST_request(self):
     response = self.client.post('/', data={'item_text': 'A new list item'})
-    self.assertIn('A new list item', response.content.decode('utf-8'))
-    self.assertTemplateUsed(response, 'home.html')
+
+    self.assertEqual(Item.objects.count(), 1, "Item was not saved into database via POST request")
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, "A new list item", "Item saved into database with wrong text")
+
+  def test_redirects_after_POST(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+
+    self.assertEqual(response.status_code, 302, 'Did not redirect after POST request on home page')
+    self.assertEqual(response['location'], '/', 'Redirect to wrong location')
+
+  def test_only_saves_item_when_necessary(self):
+    self.client.get('/')
+    self.assertEqual(Item.objects.count(), 0, "Saved item in a GET request to home page")
+
+  def test_displays_all_list_items(self):
+    Item.objects.create(text='itemey 1')
+    Item.objects.create(text='itemey 2')
+
+    response = self.client.get('/')
+
+    self.assertIn('itemey 1', response.content.decode(), "Created item is not rendered on home page")
+    self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
@@ -38,3 +60,4 @@ class ItemModelTest(TestCase):
 
     self.assertEqual(first_saved_item.text, 'The first (ever) list item')
     self.assertEqual(second_saved_item.text, 'Item the second')
+  
