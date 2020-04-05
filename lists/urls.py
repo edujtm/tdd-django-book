94409cdc513c08866c4fd7687f3567641f2a6bab
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-04 22:16:19 -0300

    moving lists urls to correct subdirectory

--- lists/urls.py
+++ lists/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path
 from lists import views
-from lists import urls as list_urls
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/', include(list_urls))
+    path('new', views.new_list, name='new_list'),
+    path('<int:pk>/', views.view_list, name='view_list'),
+    path('<int:pk>/add_item', views.add_item, name='add_item'),
 ]
