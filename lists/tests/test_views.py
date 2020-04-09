da2170918ba9f408618b6a905b21b5bbfe3d1d93
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-09 17:52:41 -0300

    Separating unit tests into multiple files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,9 +1,5 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 
-from lists.views import home_page
 from lists.models import Item, List
 
 
@@ -13,37 +9,6 @@ class SmokeTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2, 'Could not save multiple itens on database')
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item',
-                         'to-do item was saved with incorrect text')
-        self.assertEqual(first_saved_item.list, list_, 'to-do item was not saved into correct list')
-        self.assertEqual(second_saved_item.text, 'Item the second', 'to-do item was saved with incorrect text')
-        self.assertEqual(second_saved_item.list, list_, 'to-do item was not saved into correct list')
 
 
 class ListViewTest(TestCase):
