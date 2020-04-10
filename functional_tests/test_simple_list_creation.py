cd260219a5d13d40685a8c60e343d402f5296e88
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-10 00:25:17 -0300

    Changed inputbox id and name according to the form value. FT still not passing

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -18,7 +18,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn("To-Do", header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -34,7 +34,7 @@ class NewVisitorTest(FunctionalTest):
         # item. Ela insere "Usar penas de pavão para fazer uma isca"
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
@@ -47,7 +47,7 @@ class NewVisitorTest(FunctionalTest):
         # Edith inicia uma nova lista de tarefas
         self.browser.get(self.live_server_url)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -71,7 +71,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis inicia uma nova lista inserind um item novo. Ele é menos interessante
         # que edith
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -38,14 +38,14 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1, "Item was not saved into database via POST request")
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item", "Item saved into database with wrong text")
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'text': 'A new list item'})
         self.assertRedirects(response, '/lists/1/')
 
     def test_can_save_a_POST_to_an_existing_list(self):
@@ -54,7 +54,7 @@ class ListViewTest(TestCase):
 
         self.client.post(
             f'/lists/{correct_list.id}/',
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
         )
 
         self.assertEqual(Item.objects.count(), 1, "Item was not saved into database via POST request")
@@ -68,7 +68,7 @@ class ListViewTest(TestCase):
 
         response = self.client.post(
             f'/lists/{correct_list.id}/',
-            data={'item_text': 'A new list item'}
+            data={'text': 'A new list item'}
         )
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
@@ -78,7 +78,7 @@ class ListViewTest(TestCase):
 
         response = self.client.post(
             f'/lists/{list_.id}/',
-            data={'item_text': ''}
+            data={'text': ''}
         )
 
         self.assertTemplateUsed(response, 'list.html')
@@ -97,13 +97,13 @@ class NewListTest(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
-        self.client.post('/lists/new', data={'item_text': ''})
+        self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0, "New list was created when blank input was typed")
         self.assertEqual(Item.objects.count(), 0, "Item with blank input was saved into database")
