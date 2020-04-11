c3c9a490edce7c767ade998ca12fbb0a52ff7365
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-11 00:40:04 -0300

    Fixing views so they use ExistingListItemForm

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ItemForm, ExistingListItemForm
 
 
 def home_page(request):
@@ -21,10 +21,10 @@ def new_list(request):
 
 def view_list(request, pk):
     list_ = List.objects.get(id=pk)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=list_)
 
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
             form.save_for_list(list_)
             return redirect(list_)
