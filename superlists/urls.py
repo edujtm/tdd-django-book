78581b4e67544010f089419cead41afa1b4152ec
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-04-14 22:43:44 -0300

    De-spiking the authentication system

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.urls import path, include
 from lists import views
 from lists import urls as list_urls
+from accounts import urls as auth_urls
 
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('lists/', include(list_urls)),
+    path('accounts/', include(auth_urls)),
 ]
